04ff915043b618e2fdc85c7a4ee59241a4b82521
KMyles <115660447+KMyles@users.noreply.github.com>
2022-10-17 13:06:44 -0400

    Add files via upload
    updated files for SDR

--- SDR001.ino
+++ SDR001.ino
@@ -1,5 +1,5 @@
-#include <Wire.h> 
-#include "display.h"
+#include <Wire.h> //we nneed the wire.h library for I2C communication
+#include "display.h" //WWe want to include our display code 
 
 volatile uint32_t Freq =7035001; // default power up frequency//////////
 volatile uint32_t Step =100; // default power up step//////////
@@ -8,9 +8,9 @@ volatile uint32_t Step =100; // default power up step//////////
 
 void setup() {
 
-  displayInit();
+  displayInit();  //lets set up or initialze our display 
 
-   displayFreq();
+   displayFreq(); //lets display a freq 
 }
 
 void loop() {
--- display.h
+++ display.h
@@ -1,18 +1,18 @@
  
-#include "LiquidCrystal_I2C.h"
+#include "LiquidCrystal_I2C.h"  //need the glue for the display
 
-extern volatile uint32_t Freq;
+extern volatile uint32_t Freq;  //tell the display.h file that the freq is outside of its file 
 
-LiquidCrystal_I2C lcd(0x27,40,2);  //set the LCD address to 0x27(PCF8574T)
+LiquidCrystal_I2C lcd(0x27,40,2);  //set the LCD address to 0x27(PCF8574T) //set up the LCD object
 
 void displayInit()
 {
     lcd.init();                      // initialize the lcd 
-    lcd.backlight();
+    lcd.backlight();                 //turn on the backlight 
 }
 
 void displayFreq()
 {
-   lcd.print(Freq);
+   lcd.print(Freq);                 //Display the global VAriable Freq
 }
 
